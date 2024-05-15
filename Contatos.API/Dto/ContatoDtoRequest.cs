d2aaeda20c61fd9cb982c924b92ec9755747d7d9
Paulo Silva <paulo.silva@faciltech.com.br>
2024-05-15 20:59:10 -0300

    fix: Movendo annotations de validação para local correto

--- Contatos.API/Dto/ContatoDtoRequest.cs
+++ Contatos.API/Dto/ContatoDtoRequest.cs
@@ -1,4 +1,5 @@
-﻿using Contatos.API.Interfaces;
+﻿using System.ComponentModel.DataAnnotations;
+using Contatos.API.Interfaces;
 using Contatos.API.Models;
 
 namespace Contatos.API.Dto
@@ -7,12 +8,21 @@ namespace Contatos.API.Dto
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres.")]
         public required string Nome { get; set; }
 
+        [Required(ErrorMessage = "O contato deve ser vinculado ao DDD de uma região")]
         public required int DDD { get; set; }
 
+        [StringLength(20, ErrorMessage = "O telefone deve ter no máximo 20 caracteres.")]
+        [Phone(ErrorMessage = "O número de telefone não é válido.")]
+        [Required(ErrorMessage = "O número de telefone deve ser informado")]
         public required string Telefone { get; set; }
 
+        [EmailAddress(ErrorMessage = "O email não é válido.")]
+        [StringLength(100, ErrorMessage = "O email deve ter no máximo 100 caracteres.")]
+        [Required(ErrorMessage = "O número de telefone deve ser informado")]
         public required string Email { get; set; }
 
         public static implicit operator Contato(ContatoDtoRequest contatoDtoRequest)
